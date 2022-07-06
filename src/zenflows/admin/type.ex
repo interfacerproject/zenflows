08df969b398e63e0a734eac52eb70618befbe7d6
srfsh <dev@srf.sh>
2022-07-06 14:34:39 +0200

    admin: remove passphrase requirement

--- src/zenflows/admin/type.ex
+++ src/zenflows/admin/type.ex
@@ -7,10 +7,12 @@ use Absinthe.Schema.Notation
 
 alias Zenflows.Admin.Resolv
 
+@admin_key "The configuration-defined key to authenticate admin calls."
+
 object :mutation_admin do
 	@desc "Create a Person Agent, a user."
 	field :create_user, non_null(:person) do
-		@desc "The configuration-defined key to authenticate admin calls."
+		@desc @admin_key
 		arg :admin_key, non_null(:string)
 
 		@desc "A valid email address of the user.  Must be unique."
@@ -26,6 +28,17 @@ object :mutation_admin do
 		arg :pubkeys_encoded, non_null(:string), name: "pubkeys"
 
 		resolve &Resolv.create_user/2
+	end
+
+	@desc "Import repositories from a softwarepassport instance."
+	field :import_repos, :string do
+		@desc @admin_key
+		arg :admin_key, non_null(:string)
+
+		@desc "The URL where all the repository information is listed."
+		arg :url, non_null(:string)
+
+		resolve &Resolv.import_repos/2
 	end
 end
 end

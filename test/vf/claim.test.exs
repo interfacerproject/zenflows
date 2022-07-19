9ff7a48e41e99c84695a150032af85ce32ceb81d
srfsh <dev@srf.sh>
2022-07-19 17:46:37 +0300

    test/vf/claim: style fix

--- test/vf/claim.test.exs
+++ test/vf/claim.test.exs
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 defmodule ZenflowsTest.VF.Claim do
 use ZenflowsTest.Help.EctoCase, async: true
 

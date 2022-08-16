2c8deac62dbd767a4aa4bf99cedec63dc8a2c7f2
srfsh <dev@srf.sh>
2022-08-16 13:31:06 +0300

    Zenflows{Test,}.VF.ProposedIntent: init

---
+++ src/zenflows/vf/proposed_intent/domain.ex
@@ -0,0 +1,78 @@
+# Zenflows is designed to implement the Valueflows vocabulary,
+# written and maintained by srfsh <info@dyne.org>.
+# Copyright (C) 2021-2022 Dyne.org foundation <foundation@dyne.org>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+defmodule Zenflows.VF.ProposedIntent.Domain do
+@moduledoc "Domain logic of ProposedIntents."
+
+alias Ecto.Multi
+alias Zenflows.DB.Repo
+alias Zenflows.VF.ProposedIntent
+
+@typep repo() :: Ecto.Repo.t()
+@typep chgset() :: Ecto.Changeset.t()
+@typep id() :: Zenflows.DB.Schema.id()
+@typep params() :: Zenflows.DB.Schema.params()
+
+@spec one(repo(), id()) :: {:ok, ProposedIntent.t()} | {:error, String.t()}
+def one(repo \\ Repo, id) do
+	one_by(repo, id: id)
+end
+
+@spec one_by(repo(), map() | Keyword.t())
+	:: {:ok, ProposedIntent.t()} | {:error, String.t()}
+def one_by(repo \\ Repo, clauses) do
+	case repo.get_by(ProposedIntent, clauses) do
+		nil -> {:error, "not found"}
+		found -> {:ok, found}
+	end
+end
+
+@spec create(params()) :: {:ok, ProposedIntent.t()} | {:error, chgset()}
+def create(params) do
+	Multi.new()
+	|> Multi.insert(:insert, ProposedIntent.chgset(params))
+	|> Repo.transaction()
+	|> case do
+		{:ok, %{insert: pi}} -> {:ok, pi}
+		{:error, _, cset, _} -> {:error, cset}
+	end
+end
+
+@spec delete(id())
+	:: {:ok, ProposedIntent.t()} | {:error, String.t() | chgset()}
+def delete(id) do
+	Multi.new()
+	|> Multi.put(:id, id)
+	|> Multi.run(:one, &one_by/2)
+	|> Multi.delete(:delete, &(&1.one))
+	|> Repo.transaction()
+	|> case do
+		{:ok, %{delete: pi}} -> {:ok, pi}
+		{:error, _, msg_or_cset, _} -> {:error, msg_or_cset}
+	end
+end
+
+@spec preload(ProposedIntent.t(), :published_in | :publishes)
+	:: ProposedIntent.t()
+def preload(prop_int, :published_in) do
+	Repo.preload(prop_int, :published_in)
+end
+
+def preload(prop_int, :publishes) do
+	Repo.preload(prop_int, :publishes)
+end
+end
---
+++ src/zenflows/vf/proposed_intent/resolv.ex
@@ -0,0 +1,44 @@
+# Zenflows is designed to implement the Valueflows vocabulary,
+# written and maintained by srfsh <info@dyne.org>.
+# Copyright (C) 2021-2022 Dyne.org foundation <foundation@dyne.org>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+defmodule Zenflows.VF.ProposedIntent.Resolv do
+@moduledoc "Resolvers of ProposedIntent."
+
+alias Zenflows.VF.ProposedIntent.Domain
+
+def published_in(prop_int, _, _) do
+	prop_int = Domain.preload(prop_int, :published_in)
+	{:ok, prop_int.published_in}
+end
+
+def publishes(prop_int, _, _) do
+	prop_int = Domain.preload(prop_int, :publishes)
+	{:ok, prop_int.publishes}
+end
+
+def propose_intent(params, _) do
+	with {:ok, prop_int} <- Domain.create(params) do
+		{:ok, %{proposed_intent: prop_int}}
+	end
+end
+
+def delete_proposed_intent(%{id: id}, _) do
+	with {:ok, _} <- Domain.delete(id) do
+		{:ok, true}
+	end
+end
+end

bc23a1a3150dfa3958448790fd54a0d53a2d42f4
srfsh <dev@srf.sh>
2022-08-22 20:27:48 +0300

    Zenflows{Test,}.VF.AgentRelationshipRole: add paging support and small improvements

--- src/zenflows/vf/agent_relationship_role/domain.ex
+++ src/zenflows/vf/agent_relationship_role/domain.ex
@@ -20,35 +20,48 @@ defmodule Zenflows.VF.AgentRelationshipRole.Domain do
 
 alias Ecto.Multi
 alias Zenflows.DB.Repo
+alias Zenflows.GQL.Paging
 alias Zenflows.VF.AgentRelationshipRole
 
 @typep repo() :: Ecto.Repo.t()
 @typep chgset() :: Ecto.Changeset.t()
-@typep changes() :: Ecto.Multi.changes()
 @typep id() :: Zenflows.DB.Schema.id()
 @typep params() :: Zenflows.DB.Schema.params()
 
-@spec by_id(repo(), id()) :: AgentRelationshipRole.t() | nil
-def by_id(repo \\ Repo, id) do
-	repo.get(AgentRelationshipRole, id)
+@spec one(repo(), id() | map() | Keyword.t())
+	:: {:ok, AgentRelationshipRole.t()} | {:error, String.t()}
+def one(repo \\ Repo, _)
+def one(repo, id) when is_binary(id), do: one(repo, id: id)
+def one(repo, clauses) do
+	case repo.get_by(AgentRelationshipRole, clauses) do
+		nil -> {:error, "not found"}
+		found -> {:ok, found}
+	end
+end
+
+@spec all(Paging.params()) :: Paging.result(AgentRelationshipRole.t())
+def all(params) do
+	Paging.page(AgentRelationshipRole, params)
 end
 
 @spec create(params()) :: {:ok, AgentRelationshipRole.t()} | {:error, chgset()}
 def create(params) do
 	Multi.new()
-	|> Multi.insert(:rel_role, AgentRelationshipRole.chgset(params))
+	|> Multi.insert(:insert, AgentRelationshipRole.chgset(params))
 	|> Repo.transaction()
 	|> case do
-		{:ok, %{rel_role: rr}} -> {:ok, rr}
+		{:ok, %{insert: rr}} -> {:ok, rr}
 		{:error, _, cset, _} -> {:error, cset}
 	end
 end
 
-@spec update(id(), params()) :: {:ok, AgentRelationshipRole.t()} | {:error, chgset()}
+@spec update(id(), params())
+	:: {:ok, AgentRelationshipRole.t()} | {:error, String.t() | chgset()}
 def update(id, params) do
 	Multi.new()
-	|> Multi.run(:get, multi_get(id))
-	|> Multi.update(:update, &AgentRelationshipRole.chgset(&1.get, params))
+	|> Multi.put(:id, id)
+	|> Multi.run(:one, &one/2)
+	|> Multi.update(:update, &AgentRelationshipRole.chgset(&1.one, params))
 	|> Repo.transaction()
 	|> case do
 		{:ok, %{update: rr}} -> {:ok, rr}
@@ -56,11 +69,13 @@ def update(id, params) do
 	end
 end
 
-@spec delete(id()) :: {:ok, AgentRelationshipRole.t()} | {:error, chgset()}
+@spec delete(id())
+	:: {:ok, AgentRelationshipRole.t()} | {:error, String.t() | chgset()}
 def delete(id) do
 	Multi.new()
-	|> Multi.run(:get, multi_get(id))
-	|> Multi.delete(:delete, &(&1.get))
+	|> Multi.put(:id, id)
+	|> Multi.run(:one, &one/2)
+	|> Multi.delete(:delete, &(&1.one))
 	|> Repo.transaction()
 	|> case do
 		{:ok, %{delete: rr}} -> {:ok, rr}
@@ -71,17 +86,5 @@ end
 @spec preload(AgentRelationshipRole.t(), :role_behavior) :: AgentRelationshipRole.t()
 def preload(rel_role, :role_behavior) do
 	Repo.preload(rel_role, :role_behavior)
-end
-
-# Returns an AgentRelationshipRole in ok-err tuple from given ID.
-# Used inside Ecto.Multi.run/5 to get a record in transaction.
-@spec multi_get(id()) :: (repo(), changes() -> {:ok, AgentRelationshipRole.t()} | {:error, String.t()})
-defp multi_get(id) do
-	fn repo, _ ->
-		case by_id(repo, id) do
-			nil -> {:error, "not found"}
-			rr -> {:ok, rr}
-		end
-	end
 end
 end
--- test/vf/agent_relationship_role/type.test.exs
+++ test/vf/agent_relationship_role/type.test.exs
@@ -21,95 +21,86 @@ use ZenflowsTest.Help.AbsinCase, async: true
 setup do
 	%{
 		params: %{
-			role_behavior_id: Factory.insert!(:role_behavior).id,
-			role_label: Factory.uniq("role label"),
-			inverse_role_label: Factory.uniq("inverse role label"),
-			note: Factory.uniq("note"),
+			"roleBehavior" => Factory.insert!(:role_behavior).id,
+			"roleLabel" => Factory.uniq("role label"),
+			"inverseRoleLabel" => Factory.uniq("inverse role label"),
+			"note" => Factory.uniq("note"),
 		},
-		agent_relationship_role: Factory.insert!(:agent_relationship_role),
+		inserted: Factory.insert!(:agent_relationship_role),
 	}
 end
 
-describe "Query" do
-	test "agentRelationshipRole()", %{agent_relationship_role: rel_role} do
-		assert %{data: %{"agentRelationshipRole" => data}} =
-			query!("""
-				agentRelationshipRole(id: "#{rel_role.id}") {
-					id
-					roleBehavior { id }
-					roleLabel
-					inverseRoleLabel
-					note
-				}
-			""")
+@frag """
+fragment agentRelationshipRole on AgentRelationshipRole {
+	id
+	roleBehavior { id }
+	roleLabel
+	inverseRoleLabel
+	note
+}
+"""
 
-		assert data["id"] == rel_role.id
-		assert data["roleBehavior"]["id"] == rel_role.role_behavior_id
-		assert data["roleLabel"] == rel_role.role_label
-		assert data["inverseRoleLabel"] == rel_role.inverse_role_label
-		assert data["note"] == rel_role.note
+describe "Query" do
+	test "agentRelationshipRole", %{inserted: new} do
+		assert %{data: %{"agentRelationshipRole" => data}} =
+			run!("""
+				#{@frag}
+				query ($id: ID!) {
+					agentRelationshipRole(id: $id) {...agentRelationshipRole}
+				}
+			""", vars: %{"id" => new.id})
+
+		assert data["id"] == new.id
+		assert data["roleBehavior"]["id"] == new.role_behavior_id
+		assert data["roleLabel"] == new.role_label
+		assert data["inverseRoleLabel"] == new.inverse_role_label
+		assert data["note"] == new.note
 	end
 end
 
 describe "Mutation" do
-	test "createAgentRelationshipRole()", %{params: params} do
+	test "createAgentRelationshipRole", %{params: params} do
 		assert %{data: %{"createAgentRelationshipRole" => %{"agentRelationshipRole" => data}}} =
-			mutation!("""
-				createAgentRelationshipRole(agentRelationshipRole: {
-					roleBehavior: "#{params.role_behavior_id}"
-					roleLabel: "#{params.role_label}"
-					inverseRoleLabel: "#{params.inverse_role_label}"
-					note: "#{params.note}"
-				}) {
-					agentRelationshipRole {
-						id
-						roleBehavior { id }
-						roleLabel
-						inverseRoleLabel
-						note
+			run!("""
+				#{@frag}
+				mutation ($agentRelationshipRole: AgentRelationshipRoleCreateParams!) {
+					createAgentRelationshipRole(agentRelationshipRole: $agentRelationshipRole) {
+						agentRelationshipRole {...agentRelationshipRole}
 					}
 				}
-			""")
+			""", vars: %{"agentRelationshipRole" => params})
 
 		assert {:ok, _} = Zenflows.DB.ID.cast(data["id"])
-		assert data["roleBehavior"]["id"] == params.role_behavior_id
-		assert data["roleLabel"] == params.role_label
-		assert data["inverseRoleLabel"] == params.inverse_role_label
-		assert data["note"] == params.note
+		assert data["roleBehavior"]["id"] == params["roleBehavior"]
+		assert data["roleLabel"] == params["roleLabel"]
+		assert data["inverseRoleLabel"] == params["inverseRoleLabel"]
+		assert data["note"] == params["note"]
 	end
 
-	test "updateAgentRelationshipRole()", %{params: params, agent_relationship_role: rel_role} do
+	test "updateAgentRelationshipRole", %{params: params, inserted: old} do
 		assert %{data: %{"updateAgentRelationshipRole" => %{"agentRelationshipRole" => data}}} =
-			mutation!("""
-				updateAgentRelationshipRole(agentRelationshipRole: {
-					id: "#{rel_role.id}"
-					roleBehavior: "#{params.role_behavior_id}"
-					roleLabel: "#{params.role_label}"
-					inverseRoleLabel: "#{params.inverse_role_label}"
-					note: "#{params.note}"
-				}) {
-					agentRelationshipRole {
-						id
-						roleBehavior { id }
-						roleLabel
-						inverseRoleLabel
-						note
+			run!("""
+				#{@frag}
+				mutation ($agentRelationshipRole: AgentRelationshipRoleUpdateParams!) {
+					updateAgentRelationshipRole(agentRelationshipRole: $agentRelationshipRole) {
+						agentRelationshipRole {...agentRelationshipRole}
 					}
 				}
-			""")
-
-		assert data["id"] == rel_role.id
-		assert data["roleBehavior"]["id"] == params.role_behavior_id
-		assert data["roleLabel"] == params.role_label
-		assert data["inverseRoleLabel"] == params.inverse_role_label
-		assert data["note"] == params.note
+			""", vars: %{"agentRelationshipRole" => Map.put(params, "id", old.id)})
+		assert data["id"] == old.id
+		assert data["roleBehavior"]["id"] == params["roleBehavior"]
+		assert data["roleLabel"] == params["roleLabel"]
+		assert data["inverseRoleLabel"] == params["inverseRoleLabel"]
+		assert data["note"] == params["note"]
 	end
 
-	test "deleteAgentRelationshipRole()", %{agent_relationship_role: %{id: id}} do
+	test "deleteAgentRelationshipRole()", %{inserted: %{id: id}} do
 		assert %{data: %{"deleteAgentRelationshipRole" => true}} =
-			mutation!("""
-				deleteAgentRelationshipRole(id: "#{id}")
-			""")
+			run!("""
+				mutation ($id: ID!) {
+					deleteAgentRelationshipRole(id: $id)
+				}
+			""", vars: %{"id" => id})
 	end
 end
 end

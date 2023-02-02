960ac3e53f19050adba9309e924aa89f7d3a5407
srfsh <dev@srf.sh>
2023-02-02 18:21:07 +0300

    mig: add zf_process_group

---
+++ priv/repo/migrations/20230109154532_fill_zf_process_group.exs
@@ -0,0 +1,31 @@
+# Zenflows is designed to implement the Valueflows vocabulary,
+# written and maintained by srfsh <info@dyne.org>.
+# Copyright (C) 2021-2023 Dyne.org foundation <foundation@dyne.org>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+defmodule Zenflows.SQL.Repo.Migrations.Fill_zf_process_group do
+use Ecto.Migration
+
+def change() do
+	alter table("zf_process_group") do
+		add :name, :text, null: false
+		add :note, :text
+		add :grouped_in_id, references("zf_process_group")
+		timestamps()
+	end
+
+	create constraint("zf_process_group", :check, check: "id != grouped_in_id")
+end
+end

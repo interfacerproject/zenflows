4da9f6da9359f13368868fa3e88b00b45870eb96
Alberto Lerda <albertolerda97@gmail.com>
2023-03-24 14:31:46 +0100

    test: simple test assert project creation

---
+++ test/project/domain.test.exs
@@ -0,0 +1,80 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Zenflows is software that implements the Valueflows vocabulary.
+# Zenflows is designed, written, and maintained by srfsh <srfsh@dyne.org>
+# Copyright (C) 2021-2023 Dyne.org foundation <foundation@dyne.org>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+defmodule ZenflowsTest.VF.EconomicEvent do
+use ZenflowsTest.Help.EctoCase, async: true
+
+alias Zenflows.Project.Domain
+
+setup_all do
+	[errmsg_exist_xnor: "exactly one of them must be provided"]
+end
+
+describe "`onCreate` flow" do
+	setup do
+		agent = Factory.insert!(:agent)
+		contributor = Factory.insert!(:agent)
+		title = "Test project"
+		%{params: %{
+			agent: agent,
+			contributor: contributor,
+			title: title,
+		}}
+	end
+
+	test "create a project and add a contributor", %{params: params} do
+		create_params = %{
+			title: params.title,
+			description: "ciccio",
+			link: "example.com",
+			tags: ["aa", "bbb"],
+			linked_design: "ciao",
+			location_name: "ciccio",
+			location: %{
+				lat: 13,
+				lng: 42,
+				address: "Main way"
+			},
+			location_remote: true,
+			images: [],
+			licenses: [],
+			relations: [],
+			project_type: "design",
+			contributors: [],
+			declarations: [],
+			user: params.agent,
+		}
+		{:ok, evt} = Domain.create(create_params)
+		evt = evt
+			|> Repo.preload(:output_of)
+			|> Repo.preload(:resource_inventoried_as)
+
+		assert String.contains?(evt.output_of.name, params.title)
+		assert evt.resource_inventoried_as.name == params.title
+
+		contribute_params = %{
+			contributor: params.contributor.id,
+			process: evt.output_of.id,
+			user: params.agent,
+		}
+		{:ok, _} = Domain.add_contributor(contribute_params)
+	end
+
+end
+
+end

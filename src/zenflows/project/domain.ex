679c8a817b96c4394ed1a8c8624a658628846db3
srfsh <dev@srf.sh>
2023-04-19 10:49:14 +0300

    project/domain: refactor
    * "strengths" has been renamed to "strength";
    * switch to using Decimals;
    * style fixes here and there; and
    * the code has been DRY'd up.

--- src/zenflows/project/domain.ex
+++ src/zenflows/project/domain.ex
@@ -34,20 +34,39 @@ alias Zenflows.VF.{
 }
 alias Zenflows.Wallet
 
-def idea_points(), do: %{
-	on_fork: 100,
-	on_create: 100,
-	on_contributions: 100,
-	on_accept: 100,
-	on_cite: 100,
+@spec idea_points() :: %{
+	on_fork: Decimal.t(),
+	on_create: Decimal.t(),
+	on_contributions: Decimal.t(),
+	on_accept: Decimal.t(),
+	on_cite: Decimal.t(),
 }
-def strengths_points(), do: %{
-	on_fork: 100,
-	on_create: 100,
-	on_contributions: 100,
-	on_accept: 100,
-	on_cite: 100,
+defp idea_points() do
+	%{
+		on_fork: Decimal.new("100"),
+		on_create: Decimal.new("100"),
+		on_contributions: Decimal.new("100"),
+		on_accept: Decimal.new("100"),
+		on_cite: Decimal.new("100"),
+	}
+end
+
+@spec strength_points() :: %{
+	on_fork: Decimal.t(),
+	on_create: Decimal.t(),
+	on_contributions: Decimal.t(),
+	on_accept: Decimal.t(),
+	on_cite: Decimal.t(),
 }
+defp strength_points() do
+	%{
+		on_fork: Decimal.new("100"),
+		on_create: Decimal.new("100"),
+		on_contributions: Decimal.new("100"),
+		on_accept: Decimal.new("100"),
+		on_cite: Decimal.new("100"),
+	}
+end
 
 # `maybe_create_location` creates a SpatialThing if `params.location` is
 # supplied and valid; else, it returns `nil` for the SpatialThing.
@@ -84,20 +103,29 @@ defp is_resource_design?(id) do
 	end
 end
 
-@spec create(Schema.params()) :: {:ok, EconomicEvent.t()} | {:error, Changeset.t()}
-def create(params) do
-	inst_vars = InstVars.Domain.get()
-	project_types = %{
-		design: inst_vars.specs.spec_project_design.id,
-		service: inst_vars.specs.spec_project_service.id,
-		product: inst_vars.specs.spec_project_product.id
-	}
+@spec fetch_spec_id(:design | :service | :product) :: ID.t()
+defp fetch_spec_id(:design),
+	do: InstVars.Domain.get().specs.spec_project_design.id
+defp fetch_spec_id(:service),
+	do: InstVars.Domain.get().specs.spec_project_service.id
+defp fetch_spec_id(:product),
+	do: InstVars.Domain.get().specs.spec_project_product.id
 
+@spec build_quantity() :: %{has_numerical_value: 1, has_unit_id: ID.t()}
+defp build_quantity() do
+	%{
+		has_numerical_value: 1,
+		has_unit_id: InstVars.Domain.get().units.unit_one.id,
+	}
+end
+
+@spec create(Schema.params())
+	:: {:ok, EconomicEvent.t()} | {:error, String.t() | Changeset.t()}
+def create(params) do
 	Repo.multi(fn ->
 		with {:ok, owner} <- Person.Domain.one(params.owner_id),
 				{:ok, process} <-
-					%{name: "creation of #{params.title} by #{owner.name}"}
-					|> Process.Domain.create(),
+					Process.Domain.create(%{name: "creation of #{params.title} by #{owner.name}"}),
 				{:ok, {location, remote?}} <- maybe_create_location(params),
 				# TODO: `resource_metadata` should check that the `linked_design_id` exists somehow.
 				{:ok, design?} <- is_resource_design?(params[:linked_design_id]),
@@ -109,9 +137,9 @@ def create(params) do
 						output_of_id: process.id,
 						has_point_in_time: DateTime.utc_now(),
 						resource_classified_as: params.tags,
-						resource_conforms_to_id: project_types[String.to_existing_atom(params.project_type)],
-						resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
-						to_location_id: if(location != nil, do: location.id, else: nil),
+						resource_conforms_to_id: fetch_spec_id(params.project_type),
+						resource_quantity: build_quantity(),
+						to_location_id: if(location != nil, do: location.id),
 						resource_metadata: %{
 							contributors: params.contributors,
 							licenses: params.licenses,
@@ -130,18 +158,19 @@ def create(params) do
 					}
 				),
 				# economic system: points assignments
-				{:ok} = Wallet.add_points(idea_points().on_create, params.owner_id, :idea),
-				{:ok} = Wallet.add_points(strengths_points().on_create, params.owner_id, :strengths) do
+				:ok <- Wallet.add_idea_points(params.owner_id, idea_points().on_create),
+				:ok <- Wallet.add_strength_points(params.owner_id, strength_points().on_create) do
 			{:ok, evt}
+		else
+			:error -> {:error, "couldn't add points"}
+			{:error, reason} -> {:error, reason}
 		end
 	end)
 end
 
 @spec add_contributor(Schema.params())
-	:: {:ok, EconomicEvent.t()} | {:error, Changeset.t()}
+	:: {:ok, EconomicEvent.t()} | {:error, String.t() | Changeset.t()}
 def add_contributor(params) do
-	inst_vars = InstVars.Domain.get()
-
 	Repo.multi(fn ->
 		with {:ok, evt} <- EconomicEvent.Domain.create(%{
 					action_id: "work",
@@ -149,41 +178,38 @@ def add_contributor(params) do
 					receiver_id: params.contributor_id,
 					input_of_id: params.process_id,
 					has_point_in_time: DateTime.utc_now(),
-					resource_conforms_to_id: inst_vars.specs.spec_project_design.id,
-					effort_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
+					resource_conforms_to_id: fetch_spec_id(:design),
+					effort_quantity: build_quantity(),
 				}),
 				# economic system: points assignments
-				{:ok} = Wallet.add_points(idea_points().on_contributions, params.owner_id, :idea),
-				{:ok} = Wallet.add_points(strengths_points().on_contributions, params.contributor_id, :strengths) do
-
+				:ok <- Wallet.add_idea_points(params.owner_id, idea_points().on_contributions),
+				:ok <- Wallet.add_strength_points(params.contributor_id, strength_points().on_contributions) do
 			{:ok, evt}
+		else
+			:error -> {:error, "couldn't add points"}
+			{:error, reason} -> {:error, reason}
 		end
 	end)
 end
 
 @spec fork(Schema.params())
-	:: {:ok, EconomicEvent.t()} | {:error, Changeset.t()}
+	:: {:ok, EconomicEvent.t()} | {:error, String.t() | Changeset.t()}
 def fork(params) do
-	inst_vars = InstVars.Domain.get()
-
 	Repo.multi(fn ->
 		with {:ok, owner} <- Person.Domain.one(params.owner_id),
 				{:ok, resource} <- EconomicResource.Domain.one(params.resource_id),
 				{:ok, process} <-
-					%{name: "fork of #{resource.name} by #{owner.name}"}
-					|> Process.Domain.create(),
-				{:ok, _} <- EconomicEvent.Domain.create(
-					%{
+					Process.Domain.create(%{name: "fork of #{resource.name} by #{owner.name}"}),
+				{:ok, _} <- EconomicEvent.Domain.create(%{
 						action_id: "cite",
 						input_of_id: process.id,
 						provider_id: owner.id,
 						receiver_id: owner.id,
 						has_point_in_time: DateTime.utc_now(),
 						resource_inventoried_as_id: resource.id,
-						resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
-					}
-				),
-				{:ok, forked_evt} <- EconomicEvent.Domain.create(
+						resource_quantity: build_quantity(),
+				}),
+				{:ok, forking_evt} <- EconomicEvent.Domain.create(
 					%{
 						action_id: "produce",
 						provider_id: owner.id,
@@ -192,12 +218,11 @@ def fork(params) do
 						has_point_in_time: DateTime.utc_now(),
 						resource_classified_as: resource.classified_as,
 						resource_conforms_to_id: resource.conforms_to_id,
-						resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
+						resource_quantity: build_quantity(),
 						to_location_id: resource.current_location_id,
 						resource_metadata: if(resource.metadata[:relations] != nil,
-							do: put_in(resource.metadata, [:relations],
-									resource.metadata.relations ++ [resource.id]),
-							else: nil)
+							do: put_in(resource.metadata.relations,
+								resource.metadata.relations ++ [resource.id])),
 					},
 					%{
 						name: "#{resource.name} resource forked by #{owner.name}",
@@ -206,67 +231,61 @@ def fork(params) do
 					}
 				),
 				{:ok, process_contribution} <-
-					%{name: "contribution of ${resource.name} by ${owner.name"}
-					|> Process.Domain.create(),
-				{:ok, proposal} <- Proposal.Domain.create(
-					%{name: process.name, note: params.description}
-				),
-
+					Process.Domain.create(%{name: "contribution of ${resource.name} by ${owner.name"}),
+				{:ok, proposal} <-
+					Proposal.Domain.create(%{name: process.name, note: params.description}),
 				# Propose contribution
 				{:ok, cite_resource_forked} <- Intent.Domain.create(%{
-						action_id: "cite",
-						input_of_id: process_contribution.id,
-						provider_id: resource.primary_accountable_id,
-						has_point_in_time: DateTime.utc_now(),
-						resource_inventoried_as_id: forked_evt.resource_inventoried_as_id,
-						resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
-					}),
+					action_id: "cite",
+					input_of_id: process_contribution.id,
+					provider_id: resource.primary_accountable_id,
+					has_point_in_time: DateTime.utc_now(),
+					resource_inventoried_as_id: forking_evt.resource_inventoried_as_id,
+					resource_quantity: build_quantity(),
+				}),
 				{:ok, accept_resource_origin} <- Intent.Domain.create(%{
-						action_id: "accept",
-						input_of_id: process_contribution.id,
-						receiver_id: owner.id,
-						has_point_in_time: DateTime.utc_now(),
-						resource_inventoried_as_id: resource.id,
-						resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
-					}),
+					action_id: "accept",
+					input_of_id: process_contribution.id,
+					receiver_id: owner.id,
+					has_point_in_time: DateTime.utc_now(),
+					resource_inventoried_as_id: resource.id,
+					resource_quantity: build_quantity(),
+				}),
 				{:ok, modify_resource_origin} <- Intent.Domain.create(%{
-						action_id: "modify",
-						output_of_id: process_contribution.id,
-						receiver_id: owner.id,
-						has_point_in_time: DateTime.utc_now(),
-						resource_inventoried_as_id: resource.id,
-						resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
-					}),
+					action_id: "modify",
+					output_of_id: process_contribution.id,
+					receiver_id: owner.id,
+					has_point_in_time: DateTime.utc_now(),
+					resource_inventoried_as_id: resource.id,
+					resource_quantity: build_quantity(),
+				}),
 				# Link proposal and intent
 				{:ok, _} <- ProposedIntent.Domain.create(%{
-						published_in_id: proposal.id,
-						publishes_id: cite_resource_forked.id,
-					}),
+					published_in_id: proposal.id,
+					publishes_id: cite_resource_forked.id,
+				}),
 				{:ok, _} <- ProposedIntent.Domain.create(%{
-						published_in_id: proposal.id,
-						publishes_id: accept_resource_origin.id,
-					}),
+					published_in_id: proposal.id,
+					publishes_id: accept_resource_origin.id,
+				}),
 				{:ok, _} <- ProposedIntent.Domain.create(%{
-						published_in_id: proposal.id,
-						publishes_id: modify_resource_origin.id,
-					}),
+					published_in_id: proposal.id,
+					publishes_id: modify_resource_origin.id,
+				}),
 				# economic system: points assignments
-				{:ok} = Wallet.add_points(idea_points().on_fork, resource.primary_accountable_id, :idea),
-				{:ok} = Wallet.add_points(strengths_points().on_fork, params.owner_id, :strengths) do
-
-			{:ok, %{proposal: proposal, fork_event: forked_evt}}
+				:ok <- Wallet.add_idea_points(resource.primary_accountable_id, idea_points().on_fork),
+				:ok <- Wallet.add_strength_points(params.owner_id, strength_points().on_fork) do
+			{:ok, %{proposal: proposal, forking_evt: forking_evt}}
 		end
 	end)
 end
 
 @spec cite(Schema.params())
-	:: {:ok, EconomicEvent.t()} | {:error, Changeset.t()}
+	:: {:ok, EconomicEvent.t()} | {:error, String.t() | Changeset.t()}
 def cite(params) do
-	inst_vars = InstVars.Domain.get()
-
 	Repo.multi(fn ->
 		# TODO: we should check that the `process_id` exist somehow.
-		with {:ok, resource} = EconomicResource.Domain.one(params.resource_id),
+		with {:ok, resource} <- EconomicResource.Domain.one(params.resource_id),
 				{:ok, evt} <- EconomicEvent.Domain.create(%{
 					action_id: "cite",
 					input_of_id: params.process_id,
@@ -274,13 +293,15 @@ def cite(params) do
 					receiver_id: params.owner_id,
 					has_point_in_time: DateTime.utc_now(),
 					resource_inventoried_as_id: params.resource_id,
-					resource_quantity: %{has_numerical_value: 1, has_unit_id: inst_vars.units.unit_one.id},
+					resource_quantity: build_quantity(),
 				}),
 				# economic system: points assignments
-				{:ok} = Wallet.add_points(idea_points().on_cite, params.owner_id, :idea),
-				{:ok} = Wallet.add_points(strengths_points().on_cite, resource.primary_accountable_id, :strengths) do
-
+				:ok <- Wallet.add_idea_points(params.owner_id, idea_points().on_cite),
+				:ok <- Wallet.add_strength_points(resource.primary_accountable_id, strength_points().on_cite) do
 			{:ok, evt}
+		else
+			:error -> {:error, "couldn't add points"}
+			{:error, reason} -> {:error, reason}
 		end
 	end)
 end
@@ -288,12 +309,9 @@ end
 @spec approve(Schema.params())
 	:: {:ok, EconomicEvent.t()} | {:error, Changeset.t()}
 def approve(params) do
-	inst_vars = InstVars.Domain.get()
-
 	Repo.multi(fn ->
 		with {:ok, proposal} <- Proposal.Domain.one(params.proposal_id),
 				proposal <- Repo.preload(proposal, :primary_intents) do
-
 			{:ok, proposal}
 		end
 	end)
